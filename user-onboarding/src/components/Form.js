7171a456d797f876e818ff11169a15a773bbe9b7
unknown <jethanhargrave@gmail.com>
2020-03-26 08:25:18 -0400

    set up form functions and jsx

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -35,7 +35,103 @@ function Form() {
         });
     }, [formState]);
 
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(response => {
+                setPost(response.data);
+                setFormState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    terms: "",
+                });
+            })
+            .catch(error => console.log(error.response));
+    };
 
+    const validateChange = e => {
+        yup
+        .reach(formSchema, e.target.name)
+        .validate(e.target.name === "terms" ? e.target.checked : e.target.value)
+
+        .then(valid => {
+            setErrors({
+                ...errors,
+                [e.target.name]: ""
+            });
+        })
+        .catch (err => {
+            setErrors({
+                ...errors,
+                [e.target.name]: err.errors[0]
+            });
+        });
+    };
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+            ...formState,
+            [e.target.name]:
+            e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+
+        validateChange(e);
+        setFormState(newFormData);
+    };
+
+    return (
+        <div className="formdiv">
+        <form onSubmit={formSubmit}>
+            <label htmlFor="name">
+                name
+                <input
+                    type="text"
+                    name="name"
+                    value={formState.name}
+                    onChange={inputChange}
+                />
+                {errors.name.length > 0 ? <p>{errors.name}</p> : null}
+            </label>
+            <label htmlFor="email">
+                email
+                <input
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={inputChange}
+                />
+                {errors.email.length > 0 ? <p>{errors.email}</p> : null}
+            </label>
+            <label htmlFor="password">
+                password
+                <input
+                    type="password"
+                    name="password"
+                    value={formState.password}
+                    onChange={inputChange}
+                />
+                {errors.password.length > 0 ? <p>{errors.password}</p> : null}
+            </label>
+            <label htmlFor="terms">
+                <input
+                    type="checkbox"
+                    name="terms"
+                    checked={formState.terms}
+                    onChange={inputChange}
+                />
+                Terms & Conditions
+            </label>
+            { }
+            <pre>{JSON.stringify(post.name, null, 2)}</pre>
+            <button disabled={buttonDisabled}>Submit</button>
+        </form>
+        </div>
+    );
+
+}
 
 
 
